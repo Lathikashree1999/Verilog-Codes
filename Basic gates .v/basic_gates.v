75b59fdfd289662f8ba899133af7bf46dd6fca31
Lathikashree1999 <126261663+Lathikashree1999@users.noreply.github.com>
2024-08-27 08:56:56 +0530

    Create basic_gates.v

---
+++ Basic gates .v/basic_gates.v
@@ -0,0 +1,13 @@
+module basic_gates( input a,b,
+           output and_out,or_out,not_out,
+           nand_out,nor_out,xor_out,xnor_out );
+           
+    assign and_out = a&b;
+    assign or_out = a|b;
+    assign not_out = ~b;
+    assign nand_out = ~(a&b);
+    assign nor_out = ~(a|b);
+    assign xor_out = a^b;
+    assign xnor_out = ~(a^b);
+    
+endmodule
