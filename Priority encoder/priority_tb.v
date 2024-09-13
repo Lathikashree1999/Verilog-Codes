87f5a5afcfa1b83d78d6a29ccda98e217ab8074f
Lathikashree1999 <126261663+Lathikashree1999@users.noreply.github.com>
2024-09-13 14:17:22 +0530

    Create priority_tb.v

---
+++ Priority encoder/priority_tb.v
@@ -0,0 +1,13 @@
+module priority_tb;
+    reg [7:0]d;
+    wire [2:0]y;
+    
+    priority_encoder pren(d,y);
+    
+    initial begin 
+        $display("d=%0b --> y=%0b",d,y);
+        repeat(5) begin
+            d=$random;#1;
+        end
+    end
+endmodule
