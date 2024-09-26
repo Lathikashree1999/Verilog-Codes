bbfbe747a2ff18b705f0f51c3a0c48a75d140f53
Lathikashree1999 <126261663+Lathikashree1999@users.noreply.github.com>
2024-09-26 14:32:49 +0530

    Create full subtractor in structural .v

---
+++ Full Subtractor/full subtractor in structural .v
@@ -0,0 +1,18 @@
+module full_subtractor(
+  input a,b,c,
+  output diff,borrow
+);
+  half_subtractor hs1(a,b,d1,b1);
+  half_subtractor hs2(d1,c,diff,b2);
+  or (borrow,b1,b2);
+endmodule
+
+module half_subtractor(
+  input a,b,
+  output diff,borrow
+);
+  wire n1;
+  xor(diff,a,b);
+  not(n1,a);
+  and(borrow,n1,b);
+endmodule
