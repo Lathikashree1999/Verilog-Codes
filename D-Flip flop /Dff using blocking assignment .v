c0f23bc5763e3e3a1d5c801c5e4e8c5776c44fe3
Lathikashree1999 <126261663+Lathikashree1999@users.noreply.github.com>
2024-10-01 14:39:57 +0530

    Dff using blocking assignment with a=q so different gate diagram .v

---
+++ D-Flip flop /Dff using blocking assignment .v
@@ -0,0 +1,16 @@
+module D_flipflop (
+  input clk, rst_n,
+  input d,
+  output reg q
+  );
+  
+  always@(posedge clk or negedge rst_n) begin
+    if(!rst_n) q = 0;
+    else   
+      begin 
+        q = d;
+        a = q;
+      end
+  end
+  
+endmodule
