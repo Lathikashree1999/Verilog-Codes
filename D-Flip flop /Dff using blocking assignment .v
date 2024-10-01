bd346062d6f916b4e5c6c657997bc9b445919d9c
Lathikashree1999 <126261663+Lathikashree1999@users.noreply.github.com>
2024-10-01 14:47:59 +0530

    Dff using blocking assignment .v

--- D-Flip flop /Dff using blocking assignment .v
+++ D-Flip flop /Dff using blocking assignment .v
@@ -1,16 +1,14 @@
-module D_flipflop (
+module dff_asy_blocking(
   input clk, rst_n,
   input d,
-  output reg q
+  output reg a
   );
-  
+  reg q;
   always@(posedge clk or negedge rst_n) begin
     if(!rst_n) q = 0;
-    else   
-      begin 
-        q = d;
-        a = q;
-      end
+    else begin
+          q= d;
+          a=q;
+    end
   end
-  
 endmodule
