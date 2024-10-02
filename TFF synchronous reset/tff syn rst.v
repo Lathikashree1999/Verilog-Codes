6163671ee7dfb49a991da82fe71af31cebef9ab8
Lathikashree1999 <126261663+Lathikashree1999@users.noreply.github.com>
2024-10-02 17:22:14 +0530

    Create tff syn rst.v

---
+++ TFF synchronous reset/tff syn rst.v
@@ -0,0 +1,13 @@
+module tff(
+  input clk,rst,t,
+  output reg q,
+  output q_bar
+);
+  always@(posedge clk) begin
+    if(!rst) q<=0;
+    else begin
+      q <= t?~q:q;
+    end
+  end
+  assign q_bar=~q;
+endmodule
