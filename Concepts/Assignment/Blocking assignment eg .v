dedf835fbcc9cb8ec9667315a4a9fdce90f319a6
Lathikashree1999 <126261663+Lathikashree1999@users.noreply.github.com>
2024-10-04 15:28:03 +0530

    Create Blocking assignment eg .v

---
+++ Concepts/Assignment/Blocking assignment eg .v
@@ -0,0 +1,12 @@
+module eg;   //Output At time 16 a=1,b=1
+   reg a,b;
+ 
+ initial begin
+	#10 a = 1'b0;
+	#1 b = 1'b1;
+	a = 1;
+	#5 b = a;
+    $display("At time %0t a=%d,b=%d",$time,a,b);
+ end
+ 
+endmodule
