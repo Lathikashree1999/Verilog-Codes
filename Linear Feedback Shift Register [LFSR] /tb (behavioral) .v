496bc3b47a794f2bffc4b4649484f5d895028ab8
Lathikashree1999 <126261663+Lathikashree1999@users.noreply.github.com>
2024-10-08 14:56:45 +0530

    Create tb (behavioral) .v

---
+++ Linear Feedback Shift Register [LFSR] /tb (behavioral) .v
@@ -0,0 +1,19 @@
+module tb_lfsr;
+  reg clk,rst;
+  wire [3:0] op;
+  
+  lfsr lfsr1(clk,rst,op);
+  
+  always #2 clk=~clk;
+  
+  initial begin
+    $monitor("op=%b",op);
+    clk=0;rst=1;
+    #5 rst=0;
+    #50 $finish;
+  end
+  initial begin
+    $dumpfile("waves.vcd");
+    $dumpvars;
+  end
+endmodule
