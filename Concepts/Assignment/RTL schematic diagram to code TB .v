0551df3f77add6a71125099dac85b33578660bb0
Lathikashree1999 <126261663+Lathikashree1999@users.noreply.github.com>
2024-10-08 16:22:33 +0530

    Update RTL schematic diagram to code TB .v

--- Concepts/Assignment/RTL schematic diagram to code TB .v
+++ Concepts/Assignment/RTL schematic diagram to code TB .v
@@ -1,13 +1,35 @@
-module tb;
-  reg x1,x2,x3;
-  reg clk;
-  wire g,f;
-  rtl rt(x1,x2,x3,g,f);
-  
-  always #20 clk=~clk;
-  initial begin
-    $monitor("x1=%b,x2=%b,x3=%b,g=%b,f=%b",x1,x2,x3,g,f);
-    
-    x1=1;x2=1;x3=0;
-  end
+module tb_sequential_circuit;
+
+    // Inputs
+    reg clk;
+    reg x1,x2,x3;
+
+    wire g, f;
+
+
+    eg uut (
+        .clk(clk),.x1(x1),.x2(x2), .x3(x3), .g(g),.f(f));
+    always #10 clk = ~clk;
+
+    initial begin
+ 
+        clk = 0;
+        x1 = 0;
+        x2 = 0;
+        x3 = 0;
+
+        // Apply test cases
+        #15 x1 = 1; x2 = 0; x3 = 1;  
+        #20 x1 = 1; x2 = 1; x3 = 0; 
+        #20 x1 = 0; x2 = 1; x3 = 1;
+        #20 x1 = 1; x2 = 1; x3 = 1; 
+        #20 x1 = 0; x2 = 0; x3 = 0; 
+        #40 $stop;
+    end
+
+    initial begin
+        $monitor("At time %t, x1 = %b, x2 = %b, x3 = %b, g = %b, f = %b", 
+                  $time, x1, x2, x3, g, f);
+    end
+
 endmodule
