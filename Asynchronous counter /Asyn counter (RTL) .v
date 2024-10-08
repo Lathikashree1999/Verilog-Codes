566c77c86b9c5285839d8a02faba6a8d3843b484
Lathikashree1999 <126261663+Lathikashree1999@users.noreply.github.com>
2024-10-08 20:20:09 +0530

    Update Asyn counter (RTL) .v

--- Asynchronous counter /Asyn counter (RTL) .v
+++ Asynchronous counter /Asyn counter (RTL) .v
@@ -1,7 +1,8 @@
 module jkff (
   input clk, rst_n,
   input j,k,
-  output reg q, q_bar
+  output reg q, 
+  output q_bar
   );
   
   always@(posedge clk or negedge rst_n) begin 
@@ -18,7 +19,7 @@ module jkff (
   assign q_bar = ~q;
 endmodule
 
-module updown_selector(input q, q_bar, bit up, output nclk);  
+module updown_selector(input q, q_bar, up, output nclk);  
   assign nclk = up?q_bar:q;
 endmodule
 
@@ -32,7 +33,7 @@ module asyn_counter #(parameter SIZE=4)(
   genvar g;
     jkff jk0(clk, rst_n, j, k, q[0], q_bar[0]);
   	generate
-    	for(g = 1; g<SIZE; g++) begin
+    	for(g = 1; g<SIZE; g=g+1) begin
       		updown_selector ud1(q[g-1], q_bar[g-1], up, nclk[g-1]);
       		jkff jk1(nclk[g-1], rst_n, j, k, q[g], q_bar[g]);
     	end
