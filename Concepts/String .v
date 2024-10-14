d56c5e998ce768a90f396f349387952b3a1a2185
Lathikashree1999 <126261663+Lathikashree1999@users.noreply.github.com>
2024-10-14 22:27:45 +0530

    String .v

--- Concepts/String .v
+++ Concepts/String .v
@@ -1,4 +1,4 @@
-module test;  // output value=  hel
+module test;  // output value=  hel, the values in the range must be according to the size of the string. 
   reg[13*8:1]s;
   initial begin 
     s="hello world";
