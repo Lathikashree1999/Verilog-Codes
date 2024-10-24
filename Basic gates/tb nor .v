511336ec30f4700e032f04ed70fb72e01b0b2b63
Lathikashree1999 <126261663+Lathikashree1999@users.noreply.github.com>
2024-10-24 15:21:51 +0530

    Create tb nor .v

---
+++ Basic gates/tb nor .v
@@ -0,0 +1,18 @@
+module tb;
+  reg a,b;
+  wire y;
+  
+  nor_gate gate(a,b,y);
+  
+  initial begin
+    a=0;b=0;#2;
+    a=0;b=1;#2;
+    a=1;b=0;#2;
+    a=1;b=1;#2;
+  end
+   
+  initial begin
+    $monitor("a=%b,b=%b,y=%b",a,b,y);
+  end
+  
+endmodule
