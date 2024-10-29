b2c99ef4cb3997faac6957f8d76edccfff0fd918
Lathikashree1999 <126261663+Lathikashree1999@users.noreply.github.com>
2024-10-29 19:56:07 +0530

    Create generate 60% duty cycle clock tb .v

---
+++ FSM - Finite state machine /generate 60% duty cycle clock/generate 60% duty cycle clock tb .v
@@ -0,0 +1,21 @@
+module tb;
+  reg in_clk,rst;
+  wire out_clk;
+  
+  duty_cycle_clk_60 duty(in_clk,rst,out_clk);
+  
+  initial in_clk = 0;
+  always #2 in_clk = ~in_clk;
+  
+  initial begin
+    rst = 1;
+    #10 rst =0;
+    #200 $finish;
+  end
+  
+  initial begin
+    $dumpfile("waves.vcd");
+    $dumpvars;
+  end
+endmodule
+    
