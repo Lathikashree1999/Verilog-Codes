489abbf25f6442ac56ecb9919f3dc2c5828041e0
Lathikashree1999 <126261663+Lathikashree1999@users.noreply.github.com>
2024-10-29 19:55:43 +0530

    Create generate 60% duty cycle clock .v

---
+++ FSM - Finite state machine /generate 60% duty cycle clock/generate 60% duty cycle clock .v
@@ -0,0 +1,20 @@
+module duty_cycle_clk_60(input in_clk, rst, output reg out_clk);
+  reg [3:0] counter;
+  
+  always@(posedge in_clk or posedge rst) begin
+    if(rst) begin
+      counter <= 0;
+      out_clk <= 0;
+    end
+    
+    else begin
+      counter <= counter+1;
+      if(counter<6)
+        out_clk <= 1;
+      else if(counter<10)
+        out_clk <= 0;
+      else
+        out_clk <= 0;
+    end
+  end
+endmodule
