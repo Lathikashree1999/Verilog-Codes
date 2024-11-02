d4f1e9c3148d423fa967dc9013ee0035f4178044
Lathikashree1999 <126261663+Lathikashree1999@users.noreply.github.com>
2024-11-02 23:43:51 +0530

    Create SIPO 4 bit right shift register tb .v

---
+++ Registers/SIPO 4 bit right shift register tb .v
@@ -0,0 +1,31 @@
+module tb;
+  reg clk,rst,serial_in;
+  wire [3:0] parallel_out;
+  
+  sipo_4bit sipo(clk, rst, serial_in, parallel_out);
+  
+  initial begin
+    clk = 0;
+    forever #5 clk=~clk;
+  end
+  
+  initial begin
+    rst = 1;
+    serial_in = 0;
+    
+    #10 rst = 0;
+    
+    #10 serial_in = 1;
+    #10 serial_in = 0;
+    #10 serial_in = 1;
+    #10 serial_in = 1;
+    
+    #20 $finish;
+  end
+  
+  initial begin
+    $monitor("Time %t: clk=%b, rst=%b, serial_in=%b, parallel_out=%b",$time,clk,rst,serial_in,parallel_out);
+    $dumpfile("waves.vcd");
+    $dumpvars;
+  end
+endmodule
