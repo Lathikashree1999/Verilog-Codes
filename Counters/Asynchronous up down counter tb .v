7b15c4151ddcdef3a2555075f42558eea22101d2
Lathikashree1999 <126261663+Lathikashree1999@users.noreply.github.com>
2024-11-03 22:28:30 +0530

    Create Asynchronous up down counter tb .v

---
+++ Counters/Asynchronous up down counter tb .v
@@ -0,0 +1,24 @@
+module tb_asyn_counter;
+  reg clk, rst_n;
+  reg j, k;
+  reg up;
+  wire [3:0] q, q_bar;
+  asyn_counter counter(clk, rst_n, j, k, up, q, q_bar);
+  
+  always #2 clk = ~clk;
+  initial begin
+    clk = 0; rst_n = 0; 
+    up = 1;
+    #4; rst_n = 1;
+    j = 1; k = 1;
+    #80;
+    rst_n = 0;
+    #4; rst_n = 1; up = 0;
+    #50;
+    $finish;
+  end
+
+  initial begin
+    $dumpfile("dump.vcd"); $dumpvars;
+  end
+endmodule
