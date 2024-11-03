cb99c18ed78d9e2ca8da8013d09f9d186f4c007c
Lathikashree1999 <126261663+Lathikashree1999@users.noreply.github.com>
2024-11-03 22:31:39 +0530

    Create Mod-8 Counter tb .v

---
+++ Counters/Mod-8 Counter tb .v
@@ -0,0 +1,26 @@
+module tb;
+  reg clk, rst;
+  wire [2:0] count;
+  
+  mod8_counter counter(clk,rst,count);
+  
+  initial begin
+      clk = 0;
+      forever #5 clk = ~clk;
+  end
+
+  initial begin
+    $monitor("Time = %0t, Reset = %b, Count = %b", $time, rst, count);
+      rst = 1; #10;
+      rst = 0; #80;
+      rst = 1; #10;
+      rst = 0; #40;
+      $stop;
+  end
+  
+  initial begin
+    $dumpfile("waves.vcd");
+    $dumpvars;
+  end
+  
+endmodule
