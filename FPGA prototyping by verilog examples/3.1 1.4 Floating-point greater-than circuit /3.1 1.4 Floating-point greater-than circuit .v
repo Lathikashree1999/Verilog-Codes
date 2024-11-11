92b1d5f6ce569c7f77ce028cc4cd418e36d7a8ec
Lathikashree1999 <126261663+Lathikashree1999@users.noreply.github.com>
2024-11-11 17:03:11 +0530

    Delete FPGA prototyping by verilog examples/3.1 1.4 Floating-point greater-than circuit  directory

--- FPGA prototyping by verilog examples/3.1 1.4 Floating-point greater-than circuit /3.1 1.4 Floating-point greater-than circuit .v
+++
@@ -1,8 +0,0 @@
-/*3.1 1.4 Floating-point greater-than circuit 
-A floating-point greater-than circuit compares two floating-point numbers and asserts out- 
-put, gt, when the first number is larger than the second number. Assume that the two 
-numbers are represented in the format discussed in Section 3.9.4. 
-1. Design the circuit and derive the code. 
-2. Derive a testbench and use simulation to verify operation of the code. 
-3. Design a testing circuit and derive the code. 
-4. Synthesize the circuit, program the FPGA, and verify its operation.*/
