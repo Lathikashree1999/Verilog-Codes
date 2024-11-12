67bf2fd551bfef70254cfbc676368e08d9d9e510
Lathikashree1999 <126261663+Lathikashree1999@users.noreply.github.com>
2024-11-12 18:56:38 +0530

    Create 3.1 1.3 BCD incrementor tb .v

---
+++ FPGA prototyping by verilog examples/3.1 1.3 BCD incrementor /3.1 1.3 BCD incrementor tb .v
@@ -0,0 +1,23 @@
+module tb;
+    reg [11:0]bcd_in;
+    wire [11:0]bcd_out;
+    
+    bcd_incrementor incr(bcd_in, bcd_out);
+    
+    initial begin
+        $monitor("At time %t : Input: %b, Output: %b", $time, bcd_in, bcd_out);
+        bcd_in = 12'b0000_0010_0101;//25 incremented to 26
+        #10;
+        bcd_in = 12'b0000_0001_0000;//10 incremented to 11
+        #10;
+        bcd_in = 12'b0000_0000_0000;//0 incremented to 1 
+        #10;
+        bcd_in = 12'b0001_0010_0011;//123 incremented to 124
+        #10;
+        bcd_in = 12'b0001_0010_1001;//129 incremented to 130
+        #10;
+        bcd_in = 12'b1001_1001_1001;//999 incremented to 000(overflow)
+        #10;
+        $finish;
+    end   
+endmodule
