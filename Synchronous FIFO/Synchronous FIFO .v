83bca177bc5fe02001524dc31ae50946164bf011
Lathikashree1999 <126261663+Lathikashree1999@users.noreply.github.com>
2024-11-13 21:08:15 +0530

    Update Synchronous FIFO .v

--- Synchronous FIFO/Synchronous FIFO .v
+++ Synchronous FIFO/Synchronous FIFO .v
@@ -1,39 +1,39 @@
 module sfifo(
-  input clk,reset,write_e,read_e,
+  input clk, reset, write_enable, read_enable,
   input [7:0]data_in,
   output reg [7:0]data_out,
   output wire full,empty,
-  output reg [2:0]write_ptr,read_ptr
+  output reg [2:0]write_ptr, read_ptr
    );
   reg [7:0]fifomem[7:0];
   always @(posedge clk)
     begin
       if(reset)
         begin
-          write_ptr<=0;
-          read_ptr<=0;
-          data_out=0;
+          write_ptr <= 0;
+          read_ptr <= 0;
+         data_out = 0;
         end
     end
  
   always @(posedge clk)
     begin
-      if(write_e && !full)
+      if(write_enable && !full)
         begin
-          fifomem[write_ptr]<=data_in;
-          write_ptr<=write_ptr+1;
+          fifomem[write_ptr] <= data_in;
+          write_ptr <= write_ptr+1;
         end
     end
   
   always @(posedge clk)
     begin
-      if(read_e && !empty)
+      if(read_enable && !empty)
         begin
-          data_out<=fifomem[read_ptr];
-          read_ptr<=read_ptr+1;
+          data_out <= fifomem[read_ptr];
+          read_ptr <= read_ptr+1;
         end
     end
   
-     assign full=((write_ptr+1'b1)==read_ptr);
-     assign empty=(write_ptr==read_ptr);
+     assign full = ((write_ptr+1'b1)==read_ptr);
+     assign empty = (write_ptr==read_ptr);
 endmodule
