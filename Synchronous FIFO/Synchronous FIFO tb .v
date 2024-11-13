6dd41e68dfbaa9dfaa504793c717ad70ec8375ab
Lathikashree1999 <126261663+Lathikashree1999@users.noreply.github.com>
2024-11-13 21:09:09 +0530

    Update Synchronous FIFO tb .v

--- Synchronous FIFO/Synchronous FIFO tb .v
+++ Synchronous FIFO/Synchronous FIFO tb .v
@@ -1,48 +1,49 @@
 module sfifo_tb;
-  reg clk,reset,write_e,read_e;
+  reg clk,reset,write_enable,read_enable;
   reg [7:0]data_in;
   reg [2:0]write_ptr,read_ptr;
   reg [7:0]fifo[7:0];
   wire [7:0]data_out;
   wire full,empty;
   
-  sfifo fif(clk,reset,write_e,read_e,data_in,data_out,full,empty);
+  sfifo fif(clk, reset, write_enable, read_enable, data_in, data_out, full, empty);
   
   initial begin
-    clk=0;
-    forever #5 clk=~clk;
+    clk = 0;
+    forever #5 clk = ~clk;
   end
   
   initial begin
-    reset=1;
-    write_e=0;
-    read_e=0;
-    #11
-    reset=0;
+    reset = 1;
+    write_enable = 0;
+    read_enable = 0;
+    #15
+    reset = 0;
     
-    write_e=1;
-    read_e=0;
-    data_in=8'b00000011;
-    #20
-    data_in=8'b00001001;
-    #20
-    data_in=8'b00000111;
-    #20
-    data_in=8'b00000011;
-    #20
-    data_in=8'b00001001;
-    #20
-    data_in=8'b00000111;
-    #20
-     data_in=8'b00000011;
-    #20
-    data_in=8'b00001001;
-    #20
-    data_in=8'b00010111;
-    #20
+    write_enable = 1;
+    read_enable = 0;
     
-    read_e=1;
-    write_e=0;
+    data_in = 8'b00000001;
+    #10
+    data_in = 8'b00001001;
+    #10
+    data_in = 8'b00000111;
+    #10
+    data_in = 8'b00000011;
+    #10
+    data_in = 8'b00000100;
+    #10
+    data_in = 8'b00000110;
+    #10
+    data_in = 8'b00001000;
+    #10
+    data_in = 8'b00001010;
+    #10
+    //data_in = 8'b00010111;
+   // #10
+    
+    read_enable = 1;
+    write_enable = 0;
     #200
     $finish;
   end
@@ -50,7 +51,7 @@ module sfifo_tb;
    initial begin
      $dumpfile("waves.vcd");
      $dumpvars;
-     $monitor("time=%d, clk=%d, reset=%d, write_e=%d, read_e=%d, data_in=%d, data_out=%d, full=%d, empty=%d",$time,clk,reset,write_e,read_e,data_in,data_out,full,empty);
+     $monitor("time=%d, clk=%d, reset=%d, write_enable=%d, read_enable=%d, data_in=%d, data_out=%d, full=%d, empty=%d", $time, clk, reset, write_enable, read_enable, data_in, data_out, full, empty);
   end
 
 endmodule
