c27c2501eeaf37ee8018d91578d7eafc5b5457e0
Lathikashree1999 <126261663+Lathikashree1999@users.noreply.github.com>
2024-11-20 13:39:48 +0530

    Update Synchronous FIFO .v

--- Synchronous FIFO/Synchronous FIFO .v
+++ Synchronous FIFO/Synchronous FIFO .v
@@ -3,16 +3,16 @@ module sfifo(
   input [7:0]data_in,
   output reg [7:0]data_out,
   output wire full,empty,
-  output reg [2:0]write_ptr, read_ptr
+  output reg [3:0]write_ptr, read_ptr
    );
-  reg [7:0]fifomem[7:0];
+  reg [7:0]fifomem[15:0];
   always @(posedge clk)
     begin
       if(reset)
         begin
           write_ptr <= 0;
           read_ptr <= 0;
-         data_out = 0;
+          data_out = 0;
         end
     end
  
